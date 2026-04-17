ccb8f5a2c455ee3d1a662da7ea3bc7ef8f56bac9
Haner Camilo Perea Yara <ingeniero.software.corporativo2.1@alcanosesp.com>
2026-04-16 20:39:06 -0500

    first commit

---
+++ src/test/java/com/devsenior_sala3/clinica_app/ClinicaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsenior_sala3.clinica_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
